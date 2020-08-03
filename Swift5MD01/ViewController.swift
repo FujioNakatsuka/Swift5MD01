735da4bcfb445e71f301a4b3d7f0fb45ca01d85a
Fujio Nakatsuka <com.nifty.cbm01799>
2020-08-03 23:24:47 +0900

    Initial Commit

---
+++ Swift5MD01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MD01
+//
+//  Created by 中塚富士雄 on 2020/08/03.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
